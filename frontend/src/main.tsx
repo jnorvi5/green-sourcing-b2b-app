238415796f66a2b8ee58097c6853fb58073faa29
Jerit Norville <jnorvi5@wgu.edu>
2025-12-02 04:43:06 -0500

    Fix double Router nesting causing white screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,18 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { IntercomProvider } from './context/IntercomProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
       <AuthProvider>
         <IntercomProvider>
           <App />
         </IntercomProvider>
       </AuthProvider>
-    </BrowserRouter>
   </StrictMode>,
 )
