58ae067aab9f54e6971925c2d03708d70a8a0d27
Jerit Norville <jnorvi5@wgu.edu>
2026-01-22 21:15:32 -0500

    Update msalConfig.ts

--- lib/msalConfig.ts
+++ lib/msalConfig.ts
@@ -5,7 +5,6 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: "479e2a01-70ab-4df9-baa4-560d317c3423",
     authority: "https://login.microsoftonline.com/ca4f78d4-c753-4893-9cd8-1b309922b4dc",
-    redirectUri: "https://www.greenchainz.com/login/callback",
     redirectUri: "https://greenchainz.com/login/callback",   // matches app registration
     postLogoutRedirectUri: "https://greenchainz.com",
   },
