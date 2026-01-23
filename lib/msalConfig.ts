71d7c85321c2f72454df69b55ec11c9443c82c55
Jerit Norville <jnorvi5@wgu.edu>
2026-01-22 19:28:48 -0500

    Update authority URL in msalConfig

--- lib/msalConfig.ts
+++ lib/msalConfig.ts
@@ -4,7 +4,7 @@ import { PublicClientApplication, type Configuration } from "@azure/msal-browser
 export const msalConfig: Configuration = {
   auth: {
     clientId: "479e2a01-70ab-4df9-baa4-560d317c3423",
-    authority: "https://login.microsoftonline.com/<YOUR_TENANT_ID>",
+    authority: "https://login.microsoftonline.com/ca4f78d4-c753-4893-9cd8-1b309922b4dc",
     redirectUri: "https://greenchainz.com/login/callback",   // matches app registration
     postLogoutRedirectUri: "https://greenchainz.com",
   },
